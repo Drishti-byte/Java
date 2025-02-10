14dc827f1953a4a16d6e186d25623711b6548eb1
Drishti-byte <drishtibansal06@gmail.com>
2025-02-10 16:18:25 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,10 @@
+/*program to print the sum of two numbers*/
+public class sum {
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
