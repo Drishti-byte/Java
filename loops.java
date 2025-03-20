de3e6a5ae13f217752634fd904c8ea41a59c82a5
Drishti-byte <drishtibansal06@gmail.com>
2025-03-20 18:34:24 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,11 @@
+/*program to use for loop*/
+public class loops {
+    public static void main(String args[])
+    {
+        int i;
+        for(i=0; i < 11;i++)
+        {
+            System.out.println("Value is "+i);
+        }
+    }
+}
