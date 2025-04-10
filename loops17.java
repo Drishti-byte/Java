eeece0a6f4f33d8d11e965294cc977dd22df6923
Drishti-byte <drishtibansal06@gmail.com>
2025-04-10 12:56:36 +0530

    Add files via upload

---
+++ loops17.java
@@ -0,0 +1,24 @@
+/*program to print number pyramid*/
+import java.util.*;
+public class loops17 {
+    public static void main(String args[])
+    {
+        try(Scanner sc =new Scanner(System.in))
+        {
+            System.out.println("How many rows are there:");
+            int n = sc.nextInt();
+            for(int i=1; i <= n;i++)
+            {
+                for(int j=1; j <= n-i;j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int j=1; j <= i;j++)
+                {
+                    System.out.print(i+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
