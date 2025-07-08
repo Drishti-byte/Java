76895cd7694d358ea3e9d7515fb662445cfe7400
Drishti-byte <drishtibansal06@gmail.com>
2025-07-08 11:47:26 +0530

    Add files via upload

---
+++ lab37.java
@@ -0,0 +1,14 @@
+/*program to print pattern*/
+public class lab37 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i = 1;i <= 4;i++)
+        {
+            for(j = 1;j <= i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
